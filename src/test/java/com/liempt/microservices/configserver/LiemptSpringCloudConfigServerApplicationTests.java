1c0c14fe1c3570a204c61676da5ec007605bf85c
Liem <liem.pt@aris-vn.com>
2022-07-25 13:56:32 +0700

    Add files

---
+++ src/test/java/com/liempt/microservices/configserver/LiemptSpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liempt.microservices.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiemptSpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
